e039dd87505734a9654adae8b53b6616b9c3f0b7
Sanchi363 <sanchiteotia@gmail.com>
2025-08-25 20:29:51 +0530

    Create leetcode240.cpp

---
+++ leetcode240.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int r=matrix.size();
+        int c=matrix[0].size();
+        int i=0;
+        int j=c-1;
+        int ans=0;
+        while((i>=0&&i<r)&&(j>=0&&j<c)){
+            if(matrix[i][j]==target){
+                ans=1;
+                break;
+            }
+            else if(matrix[i][j]>target){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        if(ans){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+};
